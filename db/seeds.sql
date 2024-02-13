15e9cdb47f4b13902e7c3a6782271aa365326543
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-02-13 16:52:43 -0600

    added logic to inquirer to retrieve user input

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,20 @@
 INSERT INTO department (name)
-VALUES ("Accounting"),
+VALUES ("Partners"),
+       ("Accounting"),
        ("Valuation"),
        ("Marketing"),
        ("Software Development");
        
 INSERT INTO role (title, salary, department_id)
-VALUES ("Accountant", 93000.00, 1),
-       ("Consultant", 75000.00, 2),
-       ("Chief Marketing Officer", 189000.00, 3),
-       ("Senior Developer", 115000.00, 4);
+VALUES ("Managing Partner", 750000.00, 1),
+       ("Accountant", 93000.00, 2),
+       ("Consultant", 75000.00, 3),
+       ("Chief Marketing Officer", 189000.00, 4),
+       ("Senior Developer", 115000.00, 5);
 
 INSERT INTO employee (first_name, last_name, manager_id, role_id)
-VALUES ("John", "Smith", null, 1),
-       ("Mark", "Randall", 1, 2),
-       ("Brittany", "Green", 1, 3),
-       ("Kelsey", "Brent", 3, 4);
+VALUES ("Carl", "Price", null, 1),
+       ("John", "Smith", 1, 2),
+       ("Mark", "Randall", 1, 3),
+       ("Brittany", "Green", 1, 4),
+       ("Kelsey", "Brent", 1, 5);
--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const mysql = require('mysql2');
 // Connect mysql to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // MySQL password
-      password: 'dogziggy',
-      database: 'employees_db'
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // MySQL password
+        password: 'dogziggy',
+        database: 'employees_db'
     },
     console.log(`Successfully connected to employees_db!`)
-  );
+);
 
 // Array of questions to get user input
 const questions = [
@@ -20,17 +20,112 @@ const questions = [
         type: "list",
         message: "What would you like to do?",
         name: "initialQuestion",
-        choices: ["View All Employees", "Add An Employee", "Update Employee Role", "View All Roles", "Add A Role", "View All Departments", "Add A Department", "Quit"]
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update Employee Role", "Quit"]
     },
+
+    {
+        when: input => {
+            return input.initialQuestion == "Add A Department"
+        },
+        type: "input",
+        message: "What is the name of the new department?",
+        name: "newDepartment",
+    },
+
+    {
+        when: input => {
+            return input.initialQuestion == "Add A Role"
+        },
+        type: "input",
+        message: "What is the name of the new role?",
+        name: "newRoleName",
+    },
+
+    {
+        when: input => {
+            return input.initialQuestion == "Add A Role"
+        },
+        type: "input",
+        message: "What is the salary for the new role?",
+        name: "newRoleSalary",
+    },
+
+    {
+        when: input => {
+            return input.initialQuestion == "Add A Role"
+        },
+        type: "list",
+        message: "Which department is the new role under?",
+        choices: ["Partners", "Accounting", "Valuation", "Marketing", "Software Development"], //how do i do this
+        name: "newRoleDepartment",
+    }
 ]
 
-db.query("Select * from department", (err, results) => {
-if (err) {
-    throw err;
-} else {
-    console.table(results);
-}
-});
+function renderInformation() {
+    inquirer
+        .prompt(questions)
+        .then((responses) => {
+            if (responses.initialQuestion == "View All Departments") {
+                db.query("Select * from department", (err, results) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.table(results);
+                    }
+                });
+
+            } else if (responses.initialQuestion == "View All Roles") {
+                db.query("Select * from role", (err, results) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.table(results);
+                    }
+                });
+
+            } else if (responses.initialQuestion == "View All Employees") {
+                db.query("Select * from employee", (err, results) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.table(results);
+                    }
+                });
+
+            } else if (responses.initialQuestion == "Add A Department") {
+                let insertDepartment = responses.newDepartment;
+                db.query(`INSERT INTO department (name) VALUES (?)`, insertDepartment, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(`Added ${insertDepartment} into database!`);
+                });
+
+            } else if (responses.initialQuestion == "Add A Role") {
+                let insertRole = responses.newRoleName;
+                let insertRoleSalary = responses.newRoleSalary;
+                let insertRoleDepartment = responses.newRoleDepartment;
+                db.query(`INSERT INTO role (title, salary, department_id) VALUES (?)`, insertRole, insertRoleSalary, insertRoleDepartment, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log(`Added ${insertRole} into database!`);
+                });
+
+            } else if (responses.initialQuestion == "Add An Employee") {
+                console.log("hello!")
+
+            } else if (responses.initialQuestion == "Update Employee Role") {
+                console.log("hello!")
+
+            } else {
+                console.log("hello!")
+
+            }
+        })
+};
+
+renderInformation();
 
 // do the views first
 // adds next
