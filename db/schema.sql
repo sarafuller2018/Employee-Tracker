9739c9a709af751d3bc90c6d926bc51dc3e09d29
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-02-12 22:10:50 -0600

    created seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db_db;
+CREATE DATABASE employees_db;
 
 USE employees_db;
 SELECT DATABASE();
@@ -14,7 +14,10 @@ CREATE TABLE role (
   id INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
   PRIMARY KEY (id)
 );
 
@@ -22,7 +25,10 @@ CREATE TABLE employee (
   id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
   manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
   PRIMARY KEY (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO department (id, name)
+VALUES (001, "Accounting"),
+       (002, "Valuation"),
+       (003, "Marketing"),
+       (004, "Software Development");
+       
+INSERT INTO role (id, title, salary)
+VALUES (001, "Accountant", 93000.00),
+       (002, "Consultant", 75000.00),
+       (003, "Chief Marketing Officer", 189000.00),
+       (004, "Senior Developer", 115000.00);
+
+INSERT INTO employee (id, first_name, last_name, manager_id)
+VALUES (001, "John", "Smith", 12345),
+       (002, "Mark", "Randall", 90098),
+       (003, "Brittany", "Green", 02356),
+       (004, "Kelsey", "Brent", 86421);
